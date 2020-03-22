597be7aa3af3817012e29c344019c7f526d5e548
viniciuscsreis <viniciuscsreis@gmail.com>
2020-03-22 20:14:07 -0300

    change test

--- index.test.js
+++ index.test.js
@@ -14,13 +14,15 @@ test('wait 500 ms', async() => {
     var delta = Math.abs(end - start);
     expect(delta).toBeGreaterThan(450);
 });
-
+*/
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-    process.env['INPUT_MILLISECONDS'] = 500;
+    process.env['repo-token'] = 'b39553443821220bcb259cbdef722cd71651c7c5';
+    process.env['repo-name'] = 'action-changelog-generator';
+    process.env['repo-user'] = 'viniciusCSreis';
     const ip = path.join(__dirname, 'index.js');
     console.log(cp.execSync(`node ${ip}`).toString());
-}) */
+})
 
 test('2 > 1 ', async () => {
     expect(2).toBeGreaterThan(1);
