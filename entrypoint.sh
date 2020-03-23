211f747ca3a445e2eff476d8730c38e7a1344928
viniciusCSreis <vinicius-clemente@hotmail.com>
2020-03-22 22:50:24 -0300

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,17 @@ git commit -m "changes"
 
 git remote set-url origin "https://$GITHUB_ACTOR:$1@github.com/$GITHUB_REPOSITORY"
 
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
 
 git push origin change_log
 
+curl --location --request POST "https://api.github.com/repos/$GITHUB_ACTOR/$GITHUB_REPOSITORY/pulls" \
+--header 'Content-Type: application/json' \
+--header "Authorization: Bearer $1" \
+--data-raw '{
+  "title": "Change log",
+  "body": "Generate Change Log",
+  "head": "change_log",
+  "base": "master"
+}'
