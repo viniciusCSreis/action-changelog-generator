35e65d86deb1cbd083e90af8b64fe8ad88914f50
viniciusCSreis <vinicius-clemente@hotmail.com>
2020-03-22 22:33:31 -0300

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,37 @@ echo "https://$GITHUB_ACTOR:$1@github.com/$GITHUB_REPOSITORY"
 
 git remote set-url origin "https://$GITHUB_ACTOR:$1@github.com/$GITHUB_REPOSITORY"
 
-export GITHUB_USER="$GITHUB_ACTOR"
+PR_ARG="$INPUT_PR_TITLE"
+if [[ ! -z "$PR_ARG" ]]; then
+  PR_ARG="-m \"$PR_ARG\""
 
-hub pull-request --no-edit
+  if [[ ! -z "$INPUT_PR_BODY" ]]; then
+    PR_ARG="$PR_ARG -m \"$INPUT_PR_BODY\""
+  fi
+fi
+
+if [[ ! -z "$INPUT_PR_REVIEWER" ]]; then
+  PR_ARG="$PR_ARG -r \"$INPUT_PR_REVIEWER\""
+fi
+
+if [[ ! -z "$INPUT_PR_ASSIGNEE" ]]; then
+  PR_ARG="$PR_ARG -a \"$INPUT_PR_ASSIGNEE\""
+fi
+
+if [[ ! -z "$INPUT_PR_LABEL" ]]; then
+  PR_ARG="$PR_ARG -l \"$INPUT_PR_LABEL\""
+fi
+
+if [[ ! -z "$INPUT_PR_MILESTONE" ]]; then
+  PR_ARG="$PR_ARG -M \"$INPUT_PR_MILESTONE\""
+fi
+
+COMMAND="hub pull-request \
+  -b $DESTINATION_BRANCH \
+  -h $SOURCE_BRANCH \
+  --no-edit \
+  $PR_ARG \
+  || true"
+
+echo "$COMMAND"
+sh -c "$COMMAND"
--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
-#!/bin/bash 
-docker build . -t teste -f Dockerfile_Local
-docker run teste
+#!/bin/sh
+
+export GITHUB_ACTOR=viniciusCSreis
+export GITHUB_REPOSITORY=viniciusCSreis/action-changelog-generator
+
+docker build -t testelocal -f Dockerfile_Local 
+
+
+echo $GITHUB_ACTOR
+docker run testelocal -e GITHUB_ACTOR -e GITHUB_REPOSITORY
