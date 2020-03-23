3e4a7c7a37d5122a2de93d766544175763c21bab
viniciusCSreis <vinicius-clemente@hotmail.com>
2020-03-22 23:03:13 -0300

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,12 +18,15 @@ git commit -m "changes"
 
 git push --force origin change_log
 
-curl --location --request POST "https://api.github.com/repos/$GITHUB_ACTOR/$GITHUB_REPOSITORY/pulls" \
+COMMAND="curl --location --request POST 'https://api.github.com/repos/$GITHUB_ACTOR/$GITHUB_REPOSITORY/pulls' \
 --header 'Content-Type: application/json' \
---header "Authorization: Bearer $1" \
+--header 'Authorization: Bearer $1' \
 --data-raw '{
   "title": "Change log",
   "body": "Generate Change Log",
   "head": "change_log",
   "base": "master"
 }'
+"
+echo "$COMMAND"
+sh -c "$COMMAND"
