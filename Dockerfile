6faac9dfac5702b0f0fb9ce98a92eddb644291b7
viniciusCSreis <vinicius-clemente@hotmail.com>
2020-03-22 21:52:24 -0300

    changes

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,7 @@ FROM ferrarimarco/github-changelog-generator
 
 COPY entrypoint.sh /entrypoint.sh
 
+RUN echo http://dl-cdn.alpinelinux.org/alpine/edge/testing >> /etc/apk/repositories && \
+  apk add --no-cache git hub
+
 ENTRYPOINT ["sh","/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 github_changelog_generator -u $3 -p $2
 
 ls
 pwd
 
-git checkout -b change-log
-git add .
-git commit -m "changes"
-git push origin change-log
+git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
+
+hub pull-request --no-edit
