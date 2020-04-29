5a7465643f8b43d816840fbdfdeaafb0910c0ecd
viniciusousazup <vinicius.sousa@zup.com.br>
2020-04-29 15:44:31 -0300

    base and changelog branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 str=$GITHUB_REPOSITORY
-set -f
 IFS='/'
 arrIN=($str)
+baseBranch=$2
+changelogBranch=$3
 
-github_changelog_generator -u ${arrIN[0]} -p ${arrIN[1]} --token $1 --output CHANGELOG.md --cache-file CHANGELOG-CACHE --cache-log CHANGELOG-CACHE_LOG --issues false --issues-wo-labels false --filter-by-milestone false
-
+github_changelog_generator -u ${arrIN[0]} -p ${arrIN[1]} --output CHANGELOG.md --no-issues-wo-labels --max-issues 0 --token $1 
 git config user.email "you@example.com"
 git config user.name "GitHub Bot"
-git checkout master
-git checkout -b changelog
+git checkout "$baseBranch"
+git checkout -b "$changelogBranch"
 git add CHANGELOG.md
 git remote set-url origin "https://$GITHUB_ACTOR:$1@github.com/$GITHUB_REPOSITORY"
 git commit -m "Generate Changelog"
-git push --force origin changelog
+git push --force origin "$changelogBranch"
 
 COMMAND="curl --location --request POST 'https://api.github.com/repos/$GITHUB_REPOSITORY/pulls' \
 --header 'Content-Type: application/json' \
@@ -21,8 +21,8 @@ COMMAND="curl --location --request POST 'https://api.github.com/repos/$GITHUB_RE
 --data-raw '{
   \"title\": \"Changelog\",
   \"body\": \"Generate Change Log\",
-  \"head\": \"changelog\",
-  \"base\": \"master\"
+  \"head\": \"$changelogBranch\",
+  \"base\": \"$baseBranch\"
 }'
 "
 sh -c "$COMMAND"
