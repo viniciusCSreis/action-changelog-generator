f059030ebb8cf6402d4e396ac8a2dac1afc099a6
viniciusousazup <vinicius.sousa@zup.com.br>
2020-04-29 14:15:00 -0300

    split string

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-export IFS='/'
-read -r -a array <<< "$GITHUB_REPOSITORY"
-github_changelog_generator -u ${array[0]} -p ${array[1]} --token $1 --output CHANGELOG.md --cache-file CHANGELOG-CACHE --cache-log CHANGELOG-CACHE_LOG --issues false --issues-wo-labels false --filter-by-milestone false
+str=$GITHUB_REPOSITORY
+set -f
+IFS=/
+arrIN=($str)
+
+github_changelog_generator -u ${arrIN[0]} -p ${arrIN[1]} --token $1 --output CHANGELOG.md --cache-file CHANGELOG-CACHE --cache-log CHANGELOG-CACHE_LOG --issues false --issues-wo-labels false --filter-by-milestone false
 
 git config user.email "you@example.com"
 git config user.name "GitHub Bot"
---
+++ test.sh
@@ -0,0 +1,19 @@
+# mails="viniciusCSreis/action-changelog-generator"
+# mails2=$(echo $mails | tr "/" "\n")
+# n=0
+# result[0]=""
+# for addr in $mails2
+# do
+#     result[0] = $addr
+# done
+# for addr2 in result
+# do
+#     echo "${addr2}"
+# done=
+
+str="viniciusCSreis/action-changelog-generator"
+set -f
+IFS=/
+ary=($str)
+echo "${ary[0]}"
+echo "${ary[1]}"
